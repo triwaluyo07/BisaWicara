2d4dc15cdbe5769d3d0934a5685f9f97b3a8d611
Tri Waluyo <triwaluyo432@gmail.com>
2021-05-08 21:12:05 +0700

    OnClickListener

---
+++ app/src/main/java/teknoxera/bisawicara/AboutActivity.kt
@@ -0,0 +1,11 @@
+package teknoxera.bisawicara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
---
+++ app/src/main/java/teknoxera/bisawicara/TestSkillActivity.kt
@@ -0,0 +1,11 @@
+package teknoxera.bisawicara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestSkillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_skill)
+    }
+}
