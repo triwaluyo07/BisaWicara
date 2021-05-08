64c6f29ce98bc4ea49becfce4a47b9782c72135d
Tri Waluyo <triwaluyo432@gmail.com>
2021-05-08 21:03:16 +0700

    Kamus Activity

--- app/src/main/java/teknoxera/bisawicara/KamusActivity.kt
+++ app/src/main/java/teknoxera/bisawicara/KamusActivity.kt
@@ -1,11 +1,20 @@
 package teknoxera.bisawicara
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import teknoxera.bisawicara.databinding.ActivityKamusBinding
+import teknoxera.bisawicara.databinding.ActivityMainBinding
 
 class KamusActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityKamusBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kamus)
+        binding = ActivityKamusBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
--- app/src/main/java/teknoxera/bisawicara/MainActivity.kt
+++ app/src/main/java/teknoxera/bisawicara/MainActivity.kt
@@ -1,11 +1,22 @@
 package teknoxera.bisawicara
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import teknoxera.bisawicara.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardKamus.setOnClickListener {
+            val intent = Intent(this,KamusActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
