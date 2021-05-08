bf3f7312a30ddfd81a50c11d419cfe265b717e93
Deviko Kelvin <deviko.evangelion@gmail.com>
2021-05-09 00:42:09 +0700

    Adding GridLayout RV

--- app/src/main/java/teknoxera/bisawicara/KamusActivity.kt
+++ app/src/main/java/teknoxera/bisawicara/KamusActivity.kt
@@ -1,20 +1,25 @@
 package teknoxera.bisawicara
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.cardview.widget.CardView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import teknoxera.bisawicara.databinding.ActivityKamusBinding
-import teknoxera.bisawicara.databinding.ActivityMainBinding
 
-class KamusActivity : AppCompatActivity() {
-
+class KamusActivity : AppCompatActivity()
+{
     private lateinit var binding: ActivityKamusBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         binding = ActivityKamusBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
+    private fun initRV()
+    {
+        binding.apply {
+            rvDict.layoutManager = GridLayoutManager(this@KamusActivity, 2)
+        }
     }
 }
