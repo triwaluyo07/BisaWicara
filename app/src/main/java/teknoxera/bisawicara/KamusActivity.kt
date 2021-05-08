f806446afad22480ae7ed741eb193394baf954c6
Tri Waluyo <triwaluyo432@gmail.com>
2021-05-08 20:27:51 +0700

    Kamus Activity

---
+++ app/src/main/java/teknoxera/bisawicara/KamusActivity.kt
@@ -0,0 +1,11 @@
+package teknoxera.bisawicara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KamusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kamus)
+    }
+}
