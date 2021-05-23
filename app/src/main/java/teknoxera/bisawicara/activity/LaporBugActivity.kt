f0f1a06c1cf2d2855a12a7f1706e96d26535401e
Deviko Kelvin <deviko.evangelion@gmail.com>
2021-05-24 01:11:38 +0700

    Bug Report Dynamic Message Text Field in-app feature and adding About Us temporary view

--- app/src/main/java/teknoxera/bisawicara/activity/LaporBugActivity.kt
+++ app/src/main/java/teknoxera/bisawicara/activity/LaporBugActivity.kt
@@ -2,14 +2,19 @@ package teknoxera.bisawicara.activity
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_lapor_bug.*
-import teknoxera.bisawicara.R
 import teknoxera.bisawicara.databinding.ActivityLaporBugBinding
 
 class LaporBugActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLaporBugBinding
+
+    private companion object {
+        private const val base_url = "https://wa.me/6287700535264?text="
+        private const val temp_text =
+            "Saya%20ingin%20melaporkan%20bug%20pada%20bagian%20berikut: %0A"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +22,10 @@ class LaporBugActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         btn_send.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW,
-            Uri.parse("https://wa.me/6287700535264?text=Saya%20ingin%20melaporkan%20bug%20pada%20bagian%20berikut:"))
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("${base_url}${temp_text}${input_bug.text}")
+            )
             startActivity(intent)
         }
     }
