cc3a94fad7a25d8c2464a0cb6d264ae9da2f9742
Deviko Kelvin <deviko.evangelion@gmail.com>
2021-05-24 01:46:23 +0700

    Bug Report features minor fix

--- app/src/main/java/teknoxera/bisawicara/activity/LaporBugActivity.kt
+++ app/src/main/java/teknoxera/bisawicara/activity/LaporBugActivity.kt
@@ -3,30 +3,43 @@ package teknoxera.bisawicara.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_lapor_bug.*
 import teknoxera.bisawicara.databinding.ActivityLaporBugBinding
 
-class LaporBugActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLaporBugBinding
+class LaporBugActivity : AppCompatActivity()
+{
+	private lateinit var binding : ActivityLaporBugBinding
 
-    private companion object {
-        private const val base_url = "https://wa.me/6287700535264?text="
-        private const val temp_text =
-            "Saya%20ingin%20melaporkan%20bug%20pada%20bagian%20berikut: %0A"
-    }
+	private companion object
+	{
+		private const val base_url = "https://wa.me/6287700535264?text="
+		private const val temp_text =
+			"Saya%20ingin%20melaporkan%20bug%20pada%20bagian%20berikut: %0A"
+	}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityLaporBugBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+	override fun onCreate(savedInstanceState : Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		binding = ActivityLaporBugBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-        btn_send.setOnClickListener {
-            val intent = Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("${base_url}${temp_text}${input_bug.text}")
-            )
-            startActivity(intent)
-        }
-    }
+		btn_send.setOnClickListener {
+			if (input_bug.text.toString() == "")
+			{
+				Toast.makeText(
+					this, "Dimana \"Bug\"-nya!?", Toast.LENGTH_SHORT
+				).show()
+			}
+			else
+			{
+				startActivity(
+					Intent(
+						Intent.ACTION_VIEW, Uri.parse("${base_url}${temp_text}${input_bug.text}")
+					)
+				)
+			}
+		}
+	}
 }
